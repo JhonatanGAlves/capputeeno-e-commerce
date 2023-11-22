6886eaaca639df8831f4dd9da6d0ee71b9f378ab
JhonatanGAlves <jhonatangalves96@gmail.com>
2023-11-22 16:55:14 -0300

    feat: 🧑🏻‍💻 Starting to implement the Home page with static data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,49 @@
+"use client";
+import { useState } from "react";
+
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  const [selectedTab, setSelectedTab] = useState("all-products");
+
+  const responsivePadding =
+    "px-4 min-[920px]:px-10 min-[980px]:px-20 min-[1140px]:px-40";
+
+  return (
+    <main className={`flex flex-col ${responsivePadding} py-[2.125rem]`}>
+      <div className="flex justify-between items-center">
+        <nav>
+          <ul className="flex items-center gap-10 uppercase">
+            <li
+              className={`${
+                selectedTab === "all-products"
+                  ? "font-semibold text-[--gray-800] border-[--orange-400]"
+                  : "border-transparent"
+              } pb-1 border-b-4 border-solid cursor-pointer hover:border-[--orange-400] transition-all`}
+            >
+              all products
+            </li>
+            <li
+              className={`${
+                selectedTab === "t-shirts"
+                  ? "font-semibold text-[--gray-800] border-[--orange-400]"
+                  : "border-transparent"
+              } pb-1 border-b-4 border-solid cursor-pointer hover:border-[--orange-400] transition-all`}
+            >
+              t-shirts
+            </li>
+            <li
+              className={`${
+                selectedTab === "mugs"
+                  ? "font-semibold text-[--gray-800] border-[--orange-400]"
+                  : "border-transparent"
+              } pb-1 border-b-4 border-solid cursor-pointer hover:border-[--orange-400] transition-all`}
+            >
+              mugs
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+    </main>
+  );
 }
